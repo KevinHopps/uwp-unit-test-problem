a995e599c0a95933e79e4d240d7c3f9fd85de9d2
Kevin Hopps <kjhopps@gmail.com>
2019-02-21 11:41:38 -0600

    Adding some statements to debug.

--- UnitTestApp1/UnitTest.cpp
+++ UnitTestApp1/UnitTest.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <Windows.h>
+#include <ctime>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTestApp1
@@ -10,7 +16,15 @@ namespace UnitTestApp1
     public:
         TEST_METHOD(TestMethod1)
         {
-            // TODO: Your test code here
+			std::ostringstream log;
+
+			log << "UnitTestApp1 begin" << std::endl;
+
+			char ctime_buf[32];
+			time_t now;
+			std::time(&now);
+			ctime_s(ctime_buf, sizeof(ctime_buf), &now);
+			log << ctime_buf << std::flush;
         }
     };
 }
