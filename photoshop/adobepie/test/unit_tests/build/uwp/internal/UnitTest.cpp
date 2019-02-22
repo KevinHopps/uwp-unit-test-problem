439f02bf62c38ccebb22f36ad70db1a724afc8d7
Kevin Hopps <kjhopps@gmail.com>
2019-02-22 10:20:35 -0600

    Removing boost. Still failing.

--- photoshop/adobepie/test/unit_tests/build/uwp/internal/UnitTest.cpp
+++ photoshop/adobepie/test/unit_tests/build/uwp/internal/UnitTest.cpp
@@ -8,7 +8,6 @@
 #include <ctime>
 
 #include "test_runner.hpp"
-#include "boost/filesystem.hpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -19,18 +18,7 @@ namespace UnitTestApp1
     public:
         TEST_METHOD(TestMethod1)
         {
-			auto out_path = boost::filesystem::path(std::wstring(Windows::Storage::ApplicationData::Current->LocalFolder->Path->Begin())).append("output");
-			auto resource_path = boost::filesystem::path(std::wstring(Windows::ApplicationModel::Package::Current->InstalledLocation->Path->Begin()));
-
-			auto failed_tests = run_tests(out_path.string().c_str(), resource_path.string().c_str());
-
-			boost::filesystem::path log_path = out_path / "test_log.txt";
-			std::ofstream           log(log_path.string().c_str());
-
-			std::ostringstream oss;
-			oss << "failed_tests=" << failed_tests << '\n';
-			oss << "log output is at " << log_path.string() << '\n';
-
+			auto failed_tests = run_tests(nullptr, nullptr);
 			Assert::AreEqual(failed_tests, size_t{ 0 });
 		}
     };
